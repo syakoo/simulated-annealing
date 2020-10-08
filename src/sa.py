00334b4466d8edd47b8017a3bb5fd5d2f9727fa8
syakoo <sakodata0318@gmail.com>
2020-10-08 14:29:48 +0900

    update: iter_log()

--- src/sa.py
+++ src/sa.py
@@ -82,11 +82,12 @@ class SimulatedAnnealing:
 
         return False
 
-    def iter_log(self, x: T):
+    def iter_log(self, x: T, t: int):
         """毎イテレーションで呼び出されるログ用の関数。
 
         Args:
             x (T): 離散状態
+            t (int): イテレーション
         """
         pass
 
@@ -104,7 +105,7 @@ class SimulatedAnnealing:
         x = x_start if not x_start is None else self._discrete_state_set[0]
 
         while self._cooling_schedule(t) >= self._t_end:
-            self.iter_log(x)
+            self.iter_log(x, t)
             for _ in range(self._n_per):
                 x_next: T = self._generate_perturbation(x)
                 if self._is_acceptable(x, x_next, t):
