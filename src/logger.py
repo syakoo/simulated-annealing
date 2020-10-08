4e0b34fbfadd8befb211642277315c6d4b251744
syakoo <sakodata0318@gmail.com>
2020-10-08 13:48:59 +0900

    update: change logging.basicConfig()

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 
 formatter = "[%(levelname)s] %(asctime)s %(message)s"
 
-logging.basicConfig(filename="outputs.log",
-                    level=logging.DEBUG, format=formatter)
+logging.basicConfig(filename="outputs.log", filemode="w",
+                    level=logging.INFO, format=formatter)
 
 logger = logging.getLogger(__name__)
